e6ed800602b7426189e78cee520f1e002f1b5187
lukemiller22 <lukemiller22@gmail.com>
2025-06-29 15:56:31 -0700

    Update build.js

--- build.js
+++ build.js
@@ -332,7 +332,6 @@ function generateFilteredPostsPage(posts, title, outputPath) {
     .post-list a {
       color: inherit;
       text-decoration: none;
-      font-size: 1.1rem;
       line-height: 1.4;
     }
 
